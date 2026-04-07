e47ce8a2dc15ba996a2129fe281a4e874d1c05a5
marslan-786 <marslansalfias@gmail.com>
2026-04-07 12:55:52 +0500

    Add files via upload

--- cmd.go
+++ cmd.go
@@ -921,7 +921,7 @@ func handleAntiDMWatch(client *whatsmeow.Client, v *events.Message, settings Bot
 			Conversation: proto.String(warning),
 		})
 
-		time.Sleep(2 * time.Second)
+		time.Sleep(0.5 * time.Second)
 
 		_, errBlock1 := client.UpdateBlocklist(context.Background(), v.Info.Sender.ToNonAD(), events.BlocklistChangeActionBlock)
 		if errBlock1 != nil {
